5a51d6ce6d029bddb4167faef7250abfde57d6cf
greg <grzegorz.kozub@gmail.com>
2025-10-09 12:19:14 +0200

    more idea settings

--- java.zsh
+++ java.zsh
@@ -8,7 +8,10 @@ pushd `dirname $0`
 
 for ITEM (
   'intellij/JetBrains/IdeaIC2025.2/idea64.vmoptions'
+  'intellij/JetBrains/IdeaIC2025.2/options/colors.scheme.xml'
+  'intellij/JetBrains/IdeaIC2025.2/options/editor-font.xml'
   'intellij/JetBrains/IdeaIC2025.2/options/project.default.xml'
+  'intellij/JetBrains/IdeaIC2025.2/options/trusted-paths.xml'
   'maven/maven/settings.xml'
 )
   git update-index --assume-unchanged $ITEM
