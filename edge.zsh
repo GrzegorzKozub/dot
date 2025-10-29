25caa69684084c1a0ce92b236a34cddbe1fc0720
greg <grzegorz.kozub@gmail.com>
2025-10-29 12:42:31 +0100

    edge flags

---
+++ edge.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+set -e -o verbose
+
+# links
+
+DIR=$(dirname $(realpath $0))
+
+ln -sf $DIR/flags/microsoft-edge-stable-flags.conf $XDG_CONFIG_HOME/microsoft-edge-stable-flags.conf
