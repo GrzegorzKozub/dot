9b26fab05ec88b394a02871bd61e68a2a91d0a8f
greg <grzegorz.kozub@gmail.com>
2025-12-17 07:56:53 +0100

    simplify fps limit

--- games.zsh
+++ games.zsh
@@ -5,10 +5,6 @@ set -e -o verbose
 # links
 
 stow --dir=`dirname $0` --target=$XDG_CONFIG_HOME --stow \
-  gamemode
-
-[[ -d $XDG_CONFIG_HOME/MangoHud ]] || mkdir $XDG_CONFIG_HOME/MangoHud
-ln -sf \
-  $(dirname $(realpath $0))/mangohud/MangoHud/$HOST.conf \
-  $XDG_CONFIG_HOME/MangoHud/MangoHud.conf
+  gamemode \
+  mangohud
 
