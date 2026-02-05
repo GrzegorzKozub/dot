f07c74ed38a80413c33ba88739ae6ae63f9319f5
greg <grzegorz.kozub@gmail.com>
2026-02-05 13:47:33 +0100

    lmstudio data

--- lmstudio.sh
+++ lmstudio.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -eo pipefail -ux
 
+# config
+
+DATA=/run/media/$USER/data/.data
+
+[[ -d $DATA/lmstudio ]] || mkdir "$DATA"/lmstudio
+
+[[ -e $XDG_DATA_HOME/lmstudio ]] && rm -rf "$XDG_DATA_HOME"/lmstudio
+ln -s "$DATA"/lmstudio "$XDG_DATA_HOME"/lmstudio
+
 # links
 
 stow --dir="${BASH_SOURCE%/*}" --target="$HOME" --stow \
