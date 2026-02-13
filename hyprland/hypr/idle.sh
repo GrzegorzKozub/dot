91acb08404d47115e422be879895b4129ba35330
greg <grzegorz.kozub@gmail.com>
2026-02-13 15:21:29 +0100

    ❗remove hyprland

--- hyprland/hypr/idle.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-swayidle -w \
-  timeout 60 'brightnessctl set 10% --save' resume 'brightnessctl --restore' \
-  timeout 300 'hyprctl dispatch dpms off && swaylock' resume 'hyprctl dispatch dpms on' \
-  timeout 600 'systemctl suspend' \
-  before-sleep 'pidof swaylock || swaylock'
-
