56ca47afa2286ba3f3345aca749bc66f6a13d796
greg <grzegorz.kozub@gmail.com>
2026-02-14 13:26:26 +0100

    migrate zed theme to bash

--- zed/zed/themes/gruvbox-material-flat/build.sh
+++ zed/zed/themes/gruvbox-material-flat/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
+set -eo pipefail -ux
 
 go run ./build.go
-
--- zed/zed/themes/gruvbox-material-flat/watch.sh
+++ zed/zed/themes/gruvbox-material-flat/watch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
+set -eo pipefail -ux
 
 fswatch -o ./*.json* | while read -r; do ./build.sh; done
-
