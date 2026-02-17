fdcc693efe9c59ca0c7ef4b3bd4a11452f375284
greg <grzegorz.kozub@gmail.com>
2026-02-17 14:27:11 +0100

    install goimports

--- shared.zsh
+++ shared.zsh
@@ -26,6 +26,7 @@ for PACKAGE in \
   github.com/go-delve/delve/cmd/dlv \
   github.com/haya14busa/goplay/cmd/goplay \
   github.com/josharian/impl \
+  golang.org/x/tools/cmd/goimports \
   golang.org/x/tools/gopls \
   honnef.co/go/tools/cmd/staticcheck
 do
