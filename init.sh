fcb9bb643a76b4d908892a09d4a317aceb9568aa
greg <grzegorz.kozub@gmail.com>
2026-03-01 07:42:54 +0100

    XDG_STATE_HOME

--- init.sh
+++ init.sh
@@ -15,7 +15,10 @@ git update-index --assume-unchanged tidal-hifi/tidal-hifi/config.json
 
 # env
 
+export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+export XDG_STATE_HOME=${XDG_DATA_HOME:-$HOME/.local/state}
 
 # dirs
 
