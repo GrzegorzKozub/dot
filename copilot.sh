00c84bf0b886f0c02edc866c5705a55ead0b3212
greg <grzegorz.kozub@gmail.com>
2026-03-05 07:50:41 +0100

    git ignore copilot settings

--- copilot.sh
+++ copilot.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -eo pipefail -ux
 
+# repo
+
+pushd "${BASH_SOURCE%/*}"
+
+git update-index --assume-unchanged \
+  copilot/.copilot/config.json
+
+popd
+
 # links
 
 stow --dir="${BASH_SOURCE%/*}" --target="$XDG_CONFIG_HOME" --stow \
