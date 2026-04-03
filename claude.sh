d831fc8de03e333adbb6a3d3009a9933e7cbab72
greg <grzegorz.kozub@gmail.com>
2026-04-03 14:23:46 +0200

    add some mcp servers

--- claude.sh
+++ claude.sh
@@ -23,3 +23,40 @@ fi
 
 ln -sf "$(dirname "$(realpath "$0")")"/claude/claude/settings.json \
   "$XDG_CONFIG_HOME"/claude/settings.json
+
+# mcp
+
+# shellcheck disable=SC2016
+if ! claude mcp get GitHub &> /dev/null; then
+  claude mcp add-json --scope user GitHub '{
+    "type": "http",
+    "url": "https://api.githubcopilot.com/mcp",
+    "headers": {
+      "Authorization": "Bearer ${GITHUB_TOKEN}"
+    }
+  }'
+fi
+
+if [[ $HOST =~ ^(drifter|worker)$ ]]; then
+
+  if ! claude mcp get Atlassian &> /dev/null; then
+    claude mcp add-json --scope user Atlassian '{
+      "type": "http",
+      "url": "https://mcp.atlassian.com/v1/mcp"
+    }'
+  fi
+
+  if ! claude mcp get Bedrock &> /dev/null; then
+    claude mcp add-json --scope user Bedrock '{
+      "type": "stdio",
+      "command": "uvx",
+      "args": ["awslabs.bedrock-kb-retrieval-mcp-server@latest"],
+      "env": {
+        "AWS_PROFILE": "sms_sandbox",
+        "AWS_REGION": "eu-west-1",
+        "BEDROCK_KB_RERANKING_ENABLED": "false"
+      }
+    }'
+  fi
+
+fi
