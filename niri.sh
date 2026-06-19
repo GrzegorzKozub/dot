61dd5d052a6a46e4806ed5d99435e77b0670d11d
greg <grzegorz.kozub@gmail.com>
2026-06-19 08:16:20 +0200

    default noctalia cfg

--- niri.sh
+++ niri.sh
@@ -4,6 +4,7 @@ set -eo pipefail -ux
 # links
 
 stow --dir="${BASH_SOURCE%/*}" --target="$XDG_CONFIG_HOME" --stow \
-  niri
+  niri \
+  noctalia
 
 ln -sf "$XDG_CONFIG_HOME"/niri/"$HOST".kdl "$XDG_CONFIG_HOME"/niri/host.kdl
